2b934d33bf756a581880f089e0e506f3e91dfb20
Elias_Silva <inboxsilva1998@gmail.com>
2022-04-29 11:49:14 -0300

    Setup miragejs in project

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,9 @@
-import react from "react";
 import { makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from "react"
+import { createServer } from "miragejs"
+
+let server = createServer()
+server.get("/api/users", { users: [{ id: 1, name: "Bob" }, { id: 2, name: "Mary" }] })
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -9,11 +13,28 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export const HomePage = () => {
+  let [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.users)
+      })
+  }, [])
+
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <h1>HURI</h1>
       <p>projeto provihack do agora</p>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
     </div>
   );
 };
