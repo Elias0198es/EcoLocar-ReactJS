bc1e3656baca0a6fc1246778443a02dc4d7381de
Elias_Silva <inboxsilva1998@gmail.com>
2022-04-29 16:18:40 -0300

    Add LabTabs

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import { Home } from "./pages/home";
 import {PagesRoutes} from "./routes/router";
 
+
 function App() {
-  return <PagesRoutes />;
+  return (
+
+    <PagesRoutes />
+  )
 }
 
 export default App;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,40 +1,8 @@
-import { makeStyles } from "@material-ui/core";
-import React, { useState, useEffect } from "react"
-import { createServer } from "miragejs"
-
-let server = createServer()
-server.get("/api/users", { users: [{ id: 1, name: "Bob" }, { id: 2, name: "Mary" }] })
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    background: theme.palette.primary.main,
-    color: theme.palette.secondary.main,
-  },
-}));
+import React from "react"
+import LabTabs from "../../components/header";
 
 export const HomePage = () => {
-  let [users, setUsers] = useState([])
-
-  useEffect(() => {
-    fetch("/api/users")
-      .then((res) => res.json())
-      .then((json) => {
-        setUsers(json.users)
-      })
-  }, [])
-
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
-      <h1>HURI</h1>
-      <p>projeto provihack do agora</p>
-
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-    </div>
+    <LabTabs/>
   );
 };
